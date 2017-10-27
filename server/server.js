6c97f51afa016c906589e8bc70768a1386b6c105
dotcloudbxrn <a2tonev@gmail.com>
2017-10-27 17:50:51 +0200

    Added socket io and server/client logging on/off an active connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, './../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
-
 // This is static middleware 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+var server = http.createServer(app);
+// creates a web sockets server ->
+// -> emitting and receiving events 
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected.')
+  })
+});
+server.listen(port, () => {
   console.log(`Started on port ${port}.`);
 });
